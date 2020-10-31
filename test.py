2f952fe6298232f247130c5215cfb25c20846d4a
Jonathan Evertz <jonathan.evertz@rub.de>
2020-10-31 10:38:40 +0100

    custom dataset

--- test.py
+++ test.py
@@ -13,27 +13,9 @@ import numpy as np
 import time
 import pkbar
 from models import *
+from custom_modules import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-class TensorDataset(Dataset):
-    def __init__(self, *tensors, transform=None):
-        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
-        self.tensors = tensors
-        self.transform = transform
-
-    def __getitem__(self, index):
-        im, targ = tuple(tensor[index] for tensor in self.tensors)
-        if self.transform:
-            real_transform = transforms.Compose([
-                transforms.ToPILImage(),
-                self.transform
-            ])
-            im = real_transform(im)
-        return im, targ
-
-    def __len__(self):
-        return self.tensors[0].size(0)
 
 cudnn.benchmark = True
 transform_train = transforms.Compose([
