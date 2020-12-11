05c6cbfecf097de578d3c6ab43e58e95c02d0c6c
Jonathan Evertz <jonathan.evertz@gmail.com>
2020-12-11 17:30:24 +0100

    update

--- run_server.py
+++ run_server.py
@@ -36,11 +36,11 @@ if __name__ == "__main__":
     print("Start Calculating on Device: " + str(device))
 
     epsilon = [2.0, 1.0, 0.75, 0.5, 0.25, 0.1]
-    perturbation_counts = [1.0, 0.75, 0.5, 0.25] #gibt an wieviel Prozent der Zielklasse "perturbed" sein sollen
-    loss_functions = [KLDIV, BCE, WASSERSTEIN]
+    perturbation_counts = [0.5]#[1.0, 0.75, 0.5, 0.25] #gibt an wieviel Prozent der Zielklasse "perturbed" sein sollen
+    loss_functions = [KLDIV]#, BCE, WASSERSTEIN]
     TARGET_CLASS = DEER # Target Class wird als new class erkannt während new class normal erkannt wird
     NEW_CLASS = HORSE
-    CUSTOM_BEST_IMAGE_ID = None #wenn nicht das beste bild genommen werden soll, kann man hier eine wunsch id einsetzen
+    CUSTOM_BEST_IMAGE_ID = 9035 #wenn nicht das beste bild genommen werden soll, kann man hier eine wunsch id einsetzen
     LAYER = 2 #wieviele Dense Layer abgeschnitten werden sollen
 
     SAVE_PATH = "single_"+str(class_dict[TARGET_CLASS]).lower()+"_to_"+str(class_dict[NEW_CLASS]).lower()
