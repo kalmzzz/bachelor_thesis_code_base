99b719fc15c997a0c99d5dbacbda0f874e56d5be
Jonathan Evertz <jonathan.evertz@gmail.com>
2020-12-12 09:41:24 +0100

    1layer

--- run_server.py
+++ run_server.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
                 print("############################### [ PERT_COUNT: "+str(PERT_COUNT)+" | LOSS_FN: "+str(loss_dict[LOSS_FN])+" | EPS: "+str(EPS)+" ] ###############################")
                 print("\n")
 
-                DATASET_NAME = "single_"+str(class_dict[TARGET_CLASS]).lower()+"_to_"+str(class_dict[NEW_CLASS]).lower()+"_"+str(loss_dict[LOSS_FN])+"_"+str(PERT_COUNT)+"_pertcount_"+str(EPS)+"_eps"
+                DATASET_NAME = "single_"+str(class_dict[TARGET_CLASS]).lower()+"_to_"+str(class_dict[NEW_CLASS]).lower()+"_"+str(loss_dict[LOSS_FN])+"_"+str(PERT_COUNT)+"_pertcount_"+str(EPS)+"_eps_1layer"
                 best_image_id = CUSTOM_BEST_IMAGE_ID
                 best_image_id = generate_single_image_pertubed_dataset(model_name="basic_training", output_name=DATASET_NAME, target_class=TARGET_CLASS, new_class=NEW_CLASS, EPS=EPS, ITERS=ITERS, pertube_count=PERT_COUNT, loss_fn=LOSS_FN, custom_id=CUSTOM_BEST_IMAGE_ID, device_name=device, layer_cut=LAYER)
 
